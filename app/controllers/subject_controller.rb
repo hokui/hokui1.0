1c8c3499695df90a9e1f79de2f97a7f08114524b
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-03-01 17:04:35 +0900

    create subject page partial template

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -5,6 +5,14 @@ class SubjectController < ApplicationController
   def index
   end
 
+  def searchData(sub_id)
+    @summaries=Summary.where(subject_id: sub_id).select('id, number').order('number ASC')
+    @quizzes=Quiz.where(subject_id: sub_id).select('id, number, q_a')
+    @exams=Exam.where(subject_id: sub_id).select('id, year, number, q_a')
+  end
+
   def statistics
+    @subject=Subject.find_by_page_title("statistics")
+    searchData(@subject.id)
   end
 end
