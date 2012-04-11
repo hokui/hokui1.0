0a3b2da7d5ae94e6fbbf9ca9ffe84fd086f8e669
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-04-11 23:29:21 +0900

    バグ修正

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -74,7 +74,7 @@ class SubjectController < ApplicationController
     update=SubjectUpdate.new
     update.user_id=session[:user_id]
     update.subject_id=params[:subject_id]
-    update.type='up'
+    update.type='追加'
     update.data_type='過去問'
     update.file_id=exam.id
     update.save
@@ -152,7 +152,7 @@ class SubjectController < ApplicationController
     update=SubjectUpdate.new
     update.user_id=session[:user_id]
     update.subject_id=params[:subject_id]
-    update.type='up'
+    update.type='追加'
     update.data_type='小テスト'
     update.file_id=quiz.id
     update.save
@@ -227,7 +227,7 @@ class SubjectController < ApplicationController
     update=SubjectUpdate.new
     update.user_id=session[:user_id]
     update.subject_id=params[:subject_id]
-    update.type='up'
+    update.type='追加'
     update.data_type='授業資料'
     update.file_id=summary.id
     update.save
